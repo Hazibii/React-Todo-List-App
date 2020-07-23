e9b0fa742c4b48a49a8afda4d9223be039625de7
Hazibii <tkdparallax@gmail.com>
2020-07-24 00:29:02 +0800

    Updates

---
+++ precache-manifest.bf914edf8c7e50bc9d631d7362fad0e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df6aefb06de0280b51a916aed90a6cde",
+    "url": "/React-Todo-List-App/index.html"
+  },
+  {
+    "revision": "2250d8fb70ae82c94bb8",
+    "url": "/React-Todo-List-App/static/css/main.babb4bbb.chunk.css"
+  },
+  {
+    "revision": "f3b33a566712a8656ce0",
+    "url": "/React-Todo-List-App/static/js/2.b798610f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todo-List-App/static/js/2.b798610f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2250d8fb70ae82c94bb8",
+    "url": "/React-Todo-List-App/static/js/main.7f9de87d.chunk.js"
+  },
+  {
+    "revision": "b1b35313106c24e21197",
+    "url": "/React-Todo-List-App/static/js/runtime-main.cf1d5281.js"
+  }
+]);
